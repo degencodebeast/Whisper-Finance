52650abfb934b50703d4685b1cc4ff55969b4c1e
degencodebeast <degencodebeast@protonmail.com>
2024-07-04 00:05:02 +0100

    updates

--- backend/trading_backtests/bollinger_band_bt.py
+++ backend/trading_backtests/bollinger_band_bt.py
@@ -1,79 +1,74 @@
-'''
-EMA + Bollinger Band Backtest
-Start                                     0.0                                                                      
-End                                   30775.0
-Duration                              30775.0
-Exposure Time [%]                   85.131271
-Equity Final [$]                 9440694.8155
-Equity Peak [$]                  11328562.348
-Return [%]                         844.069482
-Buy & Hold Return [%]              908.245604
-Return (Ann.) [%]                         0.0
-Volatility (Ann.) [%]                     NaN
-Sharpe Ratio                              NaN
-Sortino Ratio                             NaN
-Calmar Ratio                              0.0
-Max. Drawdown [%]                  -65.204123
-Avg. Drawdown [%]                    -3.57187
-Max. Drawdown Duration                15234.0
-Avg. Drawdown Duration             158.505208
-# Trades                               2882.0
-Win Rate [%]                        57.078418
-Best Trade [%]                      10.116798
-Worst Trade [%]                    -14.813377
-Avg. Trade [%]                       0.834167
-Max. Trade Duration                    1123.0
-Avg. Trade Duration                108.292505
-Profit Factor                        1.555826
-Expectancy [%]                       0.975947
-SQN                                  1.254341
-'''
+import pandas as pd
+import pandas_ta as ta
+import ccxt
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
 
-import numpy as np 
-import backtesting 
-import pandas_ta as pd_ta 
-import pandas as pd 
-from backtesting.lib import crossover, plot_heatmaps 
-
-class BBANDS_EMA_STRATEGY(backtesting.Strategy):
-    timeperiod = 20 
-    ema_period = 20 
+class BollingerBandsStrategy(Strategy):
+    timeframe = '1h'
+    sma_window = 20
+    bollinger_length = 20
+    bollinger_std_dev = 1 #or 2
+    size = 1
+    target = 0.05
+    max_loss = -0.1
 
     def init(self):
-        #self.upper_band, self.middle_band, self.lower_band = self.I(pd_ta.bbands, self.data.Close, timeperiod=self.timeperiod, nbdevup=2, nbdevdn=2)
-        close = pd.Series(self.data.Close)
-        bbands = pd_ta.bbands(close, length=self.timeperiod, std=2)
-        self.upper_band = bbands['BBU_' + str(self.timeperiod) + '_2.0']
-        self.middle_band = bbands['BBM_' + str(self.timeperiod) + '_2.0']
-        self.lower_band = bbands['BBL_' + str(self.timeperiod) + '_2.0']
-        #self.ema = self.I(pd_ta.ema, self.data.Close, timeperiod = self.timeperiod)
-        self.ema = pd_ta.ema(close, length=self.timeperiod)
+        # self.exchange = ccxt.bitstamp()
+        # self.symbol = 'SOL/USD'
+        # self.df = self.fetch_ohlcv()
+        # self.df['SMA'] = ta.sma(self.df['close'], length=self.sma_window)
+        # self.bbands = ta.bbands(self.df['close'], length=self.bollinger_length, std=self.bollinger_std_dev)
+        # self.df = pd.concat([self.df, self.bbands], axis=1)
+        # self.df['BandWidth'] = self.df['BBU_' + str(self.bollinger_length)] - self.df['BBL_' + str(self.bollinger_length)]
+        # self.df['Tight'] = self.df['BandWidth'] <= self.df['BandWidth'].quantile(0.2)
 
-    def next(self):# 5002862.9
-        if crossover(self.data.Close, self.upper_band) or crossover(self.ema, self.data.Close):
-            self.buy(tp=self.data.Close*1.1, sl=self.data.Close*0.95)
+        self.exchange = ccxt.bitstamp()
+        self.symbol = 'SOL/USD'
+        self.df = self.fetch_ohlcv()
+        self.df['SMA'] = ta.sma(self.df['close'], length=self.sma_window)
+        self.bbands = ta.bbands(self.df['close'], length=self.bollinger_length, std=self.bollinger_std_dev)
+        self.df = pd.concat([self.df, self.bbands], axis=1)
+        self.df['BandWidth'] = self.bbands.iloc[:, 0] - self.bbands.iloc[:, 2]
+        self.df['Tight'] = self.df['BandWidth'] <= self.df['BandWidth'].rolling(window=50).mean() * 0.9
 
-        elif crossover(self.data.Close, self.ema):
-            self.sell(size=2)
-            pass 
+    def fetch_ohlcv(self):
+        ohlcv = self.exchange.fetch_ohlcv(self.symbol, self.timeframe, limit=500)
+        df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+        df.set_index('timestamp', inplace=True)
+        return df
 
-data = pd.read_csv('SOL-USD1h100.csv',  index_col='datetime', parse_dates=True)
-# data.columns = [column.capitalize() for column in data.columns]
-data = data.dropna()
+    def next(self):
+        # if not self.position and self.df['Tight'].iloc[-1]:
+        #     if self.data.close[-1] > self.df['BBU_' + str(self.bollinger_length)].iloc[-1]:
+        #         self.buy(sl=self.data.close[-1] * (1 + self.max_loss), tp=self.data.close[-1] * (1 + self.target))
+        #     elif self.data.close[-1] < self.df['BBL_' + str(self.bollinger_length)].iloc[-1]:
+        #         self.sell(sl=self.data.close[-1] * (1 - self.max_loss), tp=self.data.close[-1] * (1 - self.target))
+
+        # elif not self.df['Tight'].iloc[-1]:
+        #     if self.position:
+        #         self.position.close()
+
+        if not self.position and self.df['Tight'].iloc[-1]:
+            if self.data.Close[-1] > self.bbands.iloc[-1, 0] * 0.998:
+                self.buy(sl=self.data.Close[-1] * (1 + self.max_loss), tp=self.data.Close[-1] * (1 + self.target))
+            elif self.data.Close[-1] < self.bbands.iloc[-1, 2] * 1.002:
+                self.sell(sl=self.data.Close[-1] * (1 - self.max_loss), tp=self.data.Close[-1] * (1 - self.target))
+        elif not self.df['Tight'].iloc[-1]:
+            if self.position:
+                self.position.close()
+
+data = pd.read_csv('SOL-USD1h100.csv', index_col='datetime', parse_dates=True)
 
 data = data.iloc[:, :5]
-
-# # Clean up the column names to capitalize the first letter of each word
-# data.columns = data.columns.str.title().str.title()
 
 # Ensure the DataFrame contains columns 'Open', 'High', 'Low', 'Close', 'Volume'
 data.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
 
+bt = Backtest(data, BollingerBandsStrategy, cash=100000, commission=0.002, exclusive_orders=True)
 
-bt = backtesting.Backtest(data, BBANDS_EMA_STRATEGY, cash=1000000, commission=.006)
-
-#stats = bt.run()
-stats = bt.optimize(maximize='Equity Final [$]', timeperiod=range(20, 60, 5))
+stats = bt.run()
 
 print(stats)
 
